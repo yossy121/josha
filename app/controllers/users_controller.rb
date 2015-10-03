9be02754c90d4baf2d18a230efc520b9e5277b01
yossy121 <yossy121.satoshi9973@gmail.com>
2015-10-03 09:52:34 +0900

    Edit Controller 20151003

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   end
 
   def show
+#    @user = User.find_by(:name => params[:name])
     @user = User.find(params[:id])
   end
 
@@ -42,6 +43,7 @@ class UsersController < ApplicationController
     end
 
     def correct_user
+#      @user = User.find_by(:name => params[:name])
       @user = User.find(params[:id])
       redirect_to root_url unless current_user?(@user)
     end
